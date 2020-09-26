a9e0eac5d7e2e2344d5b5e9ba8b9ba5e816bfc53
kailana <kailanakahawaii@gmail.com>
2020-09-26 10:42:29 -0700

    add tag

---
+++ spec/classroom.rb
@@ -0,0 +1,24 @@
+class ClassRoom 
+    def initialize(students) 
+       @students = students 
+    end 
+    
+    def list_student_names 
+       @students.map(&:name).join(',') 
+    end 
+ end
+
+
+ describe ClassRoom do 
+    it 'the list_student_names method should work correctly' do 
+       student1 = double('student') 
+       student2 = double('student') 
+       
+       #doubles act as mocks or 'fake data' for our tests
+       allow(student1).to receive(:name) { 'John Smith'} 
+       allow(student2).to receive(:name) { 'Jill Smith'} 
+       
+       cr = ClassRoom.new [student1,student2]
+       expect(cr.list_student_names).to eq('John Smith,Jill Smith') 
+    end 
+ end
